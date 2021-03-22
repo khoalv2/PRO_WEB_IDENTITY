50af2f12fa76e430010cbc323b6bbe012a21c46c
Khoalv3 <khoalv@mobix.co>
2021-03-22 17:13:33 +0700

    initcode Web Fe

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
+import { EmployeeEditComponent } from './employee/employee-edit/employee-edit.component';
+import { EmployeeDetailComponent } from './employee/employee-detail/employee-detail.component';
+import { ModalComponent } from './modal/modal.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeListComponent,
+    EmployeeEditComponent,
+    EmployeeDetailComponent,
+    ModalComponent,
+    NavMenuComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,  
+    AppRoutingModule,  
+    ReactiveFormsModule,    
+    FormsModule,    
+    HttpClientModule,   
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
